aba88e0e49c8b9e0f930a6125f49d1a6f5d33013
Andres Collazo <collazoandres2005@gmail.com>
2026-05-26 09:52:16 -0500

    Remove accidentally committed temp file

--- frontend/tmp_check_syntax.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const html = fs.readFileSync('frontend/report.html', 'utf8');
-const scriptMatch = html.match(/<script[^>]*>([\s\S]*?)<\/script>/g);
-let allJS = scriptMatch.map(m => m.replace(/<\/?script[^>]*>/g, '')).join('\n');
-
-const outPath = path.resolve('./tmp_report_check.js');
-fs.writeFileSync(outPath, allJS);
-console.log('Written to ' + outPath + ' (' + allJS.length + ' chars)');
